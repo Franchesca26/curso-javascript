903f200b75c24133d5b1dfb37c60407424f4751f
Franchesca26 <franchescaros26@gmail.com>
2023-08-27 18:17:34 +0200

    inicio del repositorio

---
+++ assets/js/app.js
@@ -0,0 +1,31 @@
+
+// Imprimir la tabla del 10
+
+// alert('Hola desde app.js');
+
+// console.log( console.log('Hola Mundo') );
+
+let a = 10,
+    b = 20,
+    c = 'julio mama teta ', 
+    d = 'y mama culo', 
+    x = a + b;
+
+const Saludo= c + d; 
+
+// console.log('%c Mis variables', 'color:red; font-weight: bold');
+// console.log( { a } );
+// console.log( { b } );
+// console.log( { c } );
+
+c = 'Hola de nuevo';
+
+// console.table({a,b,c,d,x});
+
+var miNombre = 'Franchesca';
+
+// var miNombre = 'Franchesca'; //esta se llama camelCase
+// var MiNombre = 'Franchesca'; // esta se llama PascalCase
+// var mi_nombre = 'Franchesca'; // esta se llama snake_case
+
+
