fadeeef2e91007a49ad0ecd05108888f778ad92f
Franchesca26 <franchescaros26@gmail.com>
2023-08-27 21:04:45 +0200

    se creo archivo primitivo

---
+++ assets/js/primitivos.js
@@ -0,0 +1,22 @@
+
+
+let nombre = 'julio saul';
+console.log(nombre);
+
+nombre = 'julio cabeza de sandia';
+console.log(nombre);
+
+nombre = "tio july"
+nombre = `july`
+
+console.log(typeof nombre);
+
+let numero = 2;
+console.log(typeof numero);
+
+numero = 33.089
+console.log(typeof numero);
+
+let verdad = true;
+console.log(typeof verdad);
+
