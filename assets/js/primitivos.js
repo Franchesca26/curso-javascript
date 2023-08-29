2df85174a28345efd86099b5dccc766d8ce81dc5
Franchesca <franchescaros26@gmail.com>
2023-08-29 10:53:35 -0400

    b

--- assets/js/primitivos.js
+++ assets/js/primitivos.js
@@ -1,9 +1,8 @@
 
-
 let nombre = 'pablo baez';
 console.log(nombre);
 
-nombre = 'julia vende sandia';
+nombre = 'julia perez';
 console.log(nombre);
 
 nombre = "tia july"
@@ -20,3 +19,17 @@ console.log(typeof numero);
 let verdad = true;
 console.log(typeof verdad);
 
+let superPoder;
+console.log( typeof superPoder );
+
+let soyNull = null;
+console.log( typeof soyNull );
+
+
+let symbol1 = Symbol('a');
+let symbol2 = Symbol('a');
+
+console.log( typeof symbol1 );
+
+
+console.log( symbol1 === symbol2 );
