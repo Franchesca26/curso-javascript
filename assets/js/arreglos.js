0367d8144cf520ba131e2cb5c66b5be7216e8fe5
Franchesca26 <franchescaros26@gmail.com>
2023-08-31 20:42:24 +0200

    arreglos ejemplos

--- assets/js/arreglos.js
+++ assets/js/arreglos.js
@@ -7,3 +7,11 @@
 let videoJuegos = ['Mario 3', 'Megaman', 'Chrono Trigger', "Huracan" ];
 console.log({ videoJuegos });
 
+console.log( videoJuegos[0] );
+
+let arreglosCosas = [
+    true, 
+    123,
+    'franchesca',
+    1+2,
+]
