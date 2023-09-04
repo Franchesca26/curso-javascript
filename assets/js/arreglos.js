3af404609f6a35731b1fc84c60d651cdd680e443
Franchesca <franchescaros26@gmail.com>
2023-09-04 11:35:28 -0400

    hello

--- assets/js/arreglos.js
+++ assets/js/arreglos.js
@@ -5,5 +5,15 @@
 // console.log(arr);
 
 let videoJuegos = ['Mario 3', 'Megaman', 'Chrono Trigger', "Huracan" ];
-console.log({ videoJuegos });
+console.log({videoJuegos});
+
+console.log( videoJuegos[0])
+
+let arregloCosas = [
+     true,
+     123,
+     'Franchesca',
+     1 + 2,
+]; 
+
 
