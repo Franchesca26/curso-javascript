fb6e95b3e9eb4b78c02756ebf3ae381c0ca309d1
Franchesca <franchescaros26@gmail.com>
2023-11-01 15:45:23 -0400

    funciones

---
+++ assets/js/protip-retorno.js
@@ -0,0 +1,15 @@
+
+// function crearPersona( nombre, apellido ) {
+//    return {
+//        nombre: nombre,
+//        apellido: apellido
+//    }
+//}
+
+const crearPersona = ( nombre, apellido ) => {
+    return {nombre,apellido }
+}
+
+
+const persona = crearPersona( 'Franchesca', 'Rosario' );
+console.log( persona );
