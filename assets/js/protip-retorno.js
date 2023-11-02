6e3441fd545ee465ed012693b9e0c186dce9b2e8
Franchesca <franchescaros26@gmail.com>
2023-11-02 14:53:28 -0400

    funciones

--- assets/js/protip-retorno.js
+++ assets/js/protip-retorno.js
@@ -6,10 +6,26 @@
 //    }
 //}
 
-const crearPersona = ( nombre, apellido ) => {
+const crearPersona = ( nombre, apellido ) => {  
     return {nombre,apellido }
 }
 
-
 const persona = crearPersona( 'Franchesca', 'Rosario' );
 console.log( persona );
+
+function imprimeArgumentos() {
+    console.log( arguments );
+}
+
+const imprimeArgumentos2 = ( edad, ...args ) => {
+    console.log({edad, args});
+    return args;
+}
+
+const [ casado, vivo, nombre, animal ] = imprimeArgumentos2(20, true, false, 'Pablo', 'Gato');
+console.log({ casado, vivo, nombre, animal });
+
+
+const { apellido } = crearPersona( 'Franchesca', 'Rosario' );
+console.log({ apellido });
+
