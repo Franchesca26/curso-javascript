2a2eb2937226c52dbc1cec54d3b8ae301829483a
Franchesca <franchescaros26@gmail.com>
2023-11-14 08:39:55 -0400

    funciones

---
+++ assets/js/valor-referencia.js
@@ -0,0 +1,16 @@
+
+let a = 10;
+let b = a;
+a = 55;
+
+console.log({ a, b });
+
+let pablo = { nombre: 'pablo'};
+let lucas = pablo;
+lucas.nombre = 'Lucas';
+
+
+console.log({ pablo, lucas });
+
+
+                                                                                                                                                                                                                                       
